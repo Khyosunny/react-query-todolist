d5de3d4933890462a2c2b5c08b839a5ac9a3b6fe
Khyosunny <kimhyo_0218@naver.com>
2021-08-22 05:30:10 +0900

    router 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
